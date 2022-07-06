3bbc7d2a338e135c583a8a64ce10f2b1040d8dc7
zafar <zafarhussain633@gmail.com>
2022-07-07 00:18:12 +0530

    added subtr palindrome problem

---
+++ longestSubstrPalindrom.js
@@ -0,0 +1,12 @@
+const longestPalindrome = (str)=>{
+    let longPalindrom = ""
+    for(let i=0; i<str.length; i++){
+        if(str[i]===str[str.length-1-i]){
+            longPalindrom +=str[i]
+            console.log("test")
+        }
+    }
+   return longPalindrom
+}
+
+console.log(longestPalindrome("abaab"))
