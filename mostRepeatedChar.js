1f9de6a7af142539ef7ccd9f9f6a27b08f36693b
zafar <zafarhussain633@gmail.com>
2022-07-10 10:39:54 +0530

    added check number is palindome problem

--- mostRepeatedChar.js
+++ mostRepeatedChar.js
@@ -1,20 +1,45 @@
-function mostReapeatedChar(str){
-    let resultCount = 0;
-    let string = ""
-  for(let i=0; i<str.length; i++){
-    let count=0
-      for(let j=0; j<str.length; j++){
-          if(str[i]===str[j]){
-            count++
-          }
-      }
-      if(count>resultCount){
-        resultCount = count
-        string=str[i]
-      }
+
+//soltion 1
+const  findMostRepeatedChar = (word)=>{
+ let repChar = ""
+ let currrent = 0;
+ for(let i of word){
+  let temp = 0;
+  for (let j of word){
+    if(i===j){
+      temp++;
+    }
   }
-  return string
+  if(temp>currrent){
+  currrent=temp
+    repChar = i
+  }
+ }
+ return repChar
 }
 
+
+//solution 2
+function mostReapeatedChar(str){
+  let resultCount = 0;
+  let string = ""
+for(let i=0; i<str.length; i++){
+  let count=0
+    for(let j=0; j<str.length; j++){
+        if(str[i]===str[j]){
+          count++
+        }
+    }
+    if(count>resultCount){
+      resultCount = count
+      string=str[i]
+    }
+}
+return string
+}
+
+
+
 console.log(mostReapeatedChar("zafaaar"));
+console.log( findMostRepeatedChar("abazzzzzbbzzzzzabczzzzbbabdra"));
 
--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,4 @@
+//solution 1 string
 function palindrome(string){
     let revStr = ""
     for(let char of string){
@@ -8,10 +9,8 @@ function palindrome(string){
 
 console.log(palindrome("abcba"));
 
-
-//solution 2
-
-
+ 
+//solution 2  string
 function palindrome2(string){
     let temp;
     let length = Math.floor(string.length/2)
@@ -27,9 +26,25 @@ console.log(palindrome2("apllpa"))
 
 
 
-//solution 3
+//solution 3 string
 function palindrome(string){
     return string.split("").every((char,index)=> char===string[string.length-(index+1)])
 }
 
 console.log(palindrome("abba"))
+
+
+//check palindrome of number 
+const isNumPalindrome=(n)=>{
+
+    let number = n
+    let result=0;
+
+    while(number>0){
+        result = (result*10)+number%10;
+        number=Math.floor(number/10)
+    }
+    return n===result
+}
+
+console.log(isNumPalindrome(1001,"checking with number"));
